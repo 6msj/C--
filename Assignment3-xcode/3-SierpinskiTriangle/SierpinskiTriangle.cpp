f1c331450adb4721ad630a700df6e6956e813a0e
James Nguyen <ja.nguyen@gmail.com>
2013-03-24 21:58:20 -0700

    Blha

--- Assignment3-xcode/3-SierpinskiTriangle/SierpinskiTriangle.cpp
+++ Assignment3-xcode/3-SierpinskiTriangle/SierpinskiTriangle.cpp
@@ -14,11 +14,26 @@
 using namespace std;
 
 /* Main program */
+void DrawFractal (double x, double y, double w, double h);
+void DrawTriangle (double x, double y, double w, double h);
 
 int main() {
    GWindow gw(600, 500);
-   GLine triangle_(200, 200, 200, 200);
 
    // Fill in the necessary code here
    return 0;
+}
+
+void DrawFractal (double x, double y, double w, double h) {
+    DrawTriangle(x, y, w, h);
+    if (w < .2 || h < .2) return;
+    double halfH = h/2;
+    double halfW = w/2;
+    DrawFractal(x, y, halfW, halfH); // left
+    DrawFractal(x + halfW/2, y + halfH, halfW, halfH); // top
+    DrawFractal(x + halfW, y, halfW, halfH); // right
+}
+
+void DrawTriangle (double x, double y, double w, double h) {
+
 }
---
+++ reverseString.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverseString(char word[]);
+
+int main() {
+    char word[] = "String";
+    reverseString(word)
+    return 0;
+}
+
+
+void reverseString(char word[]) {
+    for(int i=0, j=5; i<=j; i++, j--) {
+        char temp = word[i];
+        word[i] = word[j];
+        word[j] = temp;
+    }
+    printf("%s", word);
+
+}
